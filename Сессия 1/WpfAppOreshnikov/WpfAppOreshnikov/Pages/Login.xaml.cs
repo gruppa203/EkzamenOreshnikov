b45efb40c20e4a522c726dc148cee6654739f127
gruppa203 <149385686+gruppa203@users.noreply.github.com>
2023-12-08 11:06:23 +0300

    Загрузка

---
+++ Сессия 1/WpfAppOreshnikov/WpfAppOreshnikov/Pages/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppOreshnikov.Application_Data;
+
+namespace WpfAppOreshnikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_login_Click(object sender, RoutedEventArgs e)
+        {
+            var user = bd_Entities.Get_Context().polzovateli.FirstOrDefault(x => (x.login == text_login.Text) && (x.parol == text_password.Password));
+            if (user == null)
+            {
+                MessageBox.Show("Пользователь с таким логином и паролем не найден!", "Ошибка Авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                this.NavigationService.Navigate(new tovar());
+            }
+        }
+            
+    }
+}
---
+++ Сессия 1/WpfAppOreshnikov/WpfAppOreshnikov/Pages/tovar.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppOreshnikov.Application_Data;
+using WpfAppOreshnikov.Pages;
+
+namespace WpfAppOreshnikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для tovar.xaml
+    /// </summary>
+    public partial class tovar : Page
+    {
+        public tovar()
+        {
+            InitializeComponent();
+            string[] sort = { "По возрастанию", "По убыванию"};
+            sort_box.ItemsSource = sort.ToList();
+            string[] filtr = { "0-9,99%","10-14,99%","15% и более","Все диапазоны" };
+            filtr_box.ItemsSource = filtr.ToList();
+            grid_Data.ItemsSource = bd_Entities.Get_Context().sklad.ToList();
+            int kol = grid_Data.Items.Count;
+            lbl_kol.Content = kol.ToString();
+        }
+
+        private void Btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Login());
+        }
+    }
+}
